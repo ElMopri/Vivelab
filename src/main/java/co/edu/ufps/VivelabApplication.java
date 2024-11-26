352702c3234e08db1d2ca920584e54b7cddcaee7
ElMopri <alopeztriana1@gmail.com>
2024-11-26 10:45:28 -0500

    Montaje del proyecto en Spring boot

---
+++ src/main/java/co/edu/ufps/VivelabApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivelabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivelabApplication.class, args);
+	}
+
+}
