a61bb3bfd22e154dc295da498d456d3c27542d7b
ElMopri <alopeztriana1@gmail.com>
2024-11-26 22:28:42 -0500

    servicios de tipo de documento

--- src/main/java/co/edu/ufps/controllers/TipoDocumentoController.java
+++ src/main/java/co/edu/ufps/controllers/TipoDocumentoController.java
@@ -1,10 +1,52 @@
 package co.edu.ufps.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.ufps.entities.TipoDocumento;
+import co.edu.ufps.services.TipoDocumentoService;
 
 @RestController
 @RequestMapping("/api/tipo_documentos")
 public class TipoDocumentoController {
-
+	@Autowired
+	private TipoDocumentoService tipoDocumentoService;
+	
+	@GetMapping
+	public ResponseEntity<List<TipoDocumento>> list() {
+		return ResponseEntity.ok(tipoDocumentoService.list());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<TipoDocumento> get(@PathVariable Integer id) {
+		return ResponseEntity.ok(tipoDocumentoService.get(id));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<TipoDocumento> create(@RequestBody TipoDocumento tipoDocumento) {
+		TipoDocumento newTipoDocumento = tipoDocumentoService.create(tipoDocumento);
+		return ResponseEntity.ok(newTipoDocumento);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<TipoDocumento> update(@PathVariable Integer id, @RequestBody TipoDocumento tipoDocumentoDetails) {
+		TipoDocumento updatedTipoDocumento = tipoDocumentoService.update(id, tipoDocumentoDetails);
+		return ResponseEntity.ok(updatedTipoDocumento);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<TipoDocumento> delete(@PathVariable Integer id) {
+		TipoDocumento deletedTipoDocumento = tipoDocumentoService.delete(id);
+		return ResponseEntity.ok(deletedTipoDocumento);
+	}
 }
--- src/main/java/co/edu/ufps/services/TipoDocumentoService.java
+++ src/main/java/co/edu/ufps/services/TipoDocumentoService.java
@@ -1,8 +1,52 @@
 package co.edu.ufps.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import co.edu.ufps.entities.TipoDocumento;
+import co.edu.ufps.repositories.TipoDocumentoRepository;
 
 @Service
 public class TipoDocumentoService {
+	@Autowired
+	private TipoDocumentoRepository tipoDocumentoRepository;
 
+	public List<TipoDocumento> list() {
+		return tipoDocumentoRepository.findAll();
+	}
+	
+	public TipoDocumento get(Integer id) {
+		Optional<TipoDocumento> tipoDocumentoOpt = tipoDocumentoRepository.findById(id);
+		if (tipoDocumentoOpt.isPresent()) {
+			return tipoDocumentoOpt.get();
+		}
+		return null;
+	}
+
+	public TipoDocumento create(TipoDocumento tipoDocumento) {
+		return tipoDocumentoRepository.save(tipoDocumento);
+	}
+
+	public TipoDocumento update(Integer id, TipoDocumento tipoDocumentoDetails) {
+		Optional<TipoDocumento> tipoDocumentoOpt = tipoDocumentoRepository.findById(id);
+		if (!tipoDocumentoOpt.isPresent()) {
+			return null;
+		}
+		TipoDocumento tipoDocumento = tipoDocumentoOpt.get();
+		tipoDocumento.setDescripcion(tipoDocumentoDetails.getDescripcion());
+		return tipoDocumentoRepository.save(tipoDocumento);
+	}
+
+	public TipoDocumento delete(Integer id) {
+		Optional<TipoDocumento> tipoDocumentoOpt = tipoDocumentoRepository.findById(id);
+		if (tipoDocumentoOpt.isPresent()) {
+			TipoDocumento tipoDocumento = tipoDocumentoOpt.get();
+			tipoDocumentoRepository.delete(tipoDocumento);
+			return tipoDocumento;
+		}
+		return null;
+	}
 }
