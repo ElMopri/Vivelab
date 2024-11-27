da9d7eef41d0a20dea7a061d44fa20e9a9ed3038
ElMopri <alopeztriana1@gmail.com>
2024-11-27 08:10:24 -0500

    servicios de participante

--- src/main/java/co/edu/ufps/controllers/ParticipanteController.java
+++ src/main/java/co/edu/ufps/controllers/ParticipanteController.java
@@ -1,10 +1,52 @@
 package co.edu.ufps.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.ufps.entities.Participante;
+import co.edu.ufps.services.ParticipanteService;
 
 @RestController
 @RequestMapping("/api/participantes")
 public class ParticipanteController {
-
+	@Autowired
+	private ParticipanteService participanteService;
+	
+	@GetMapping
+	public ResponseEntity<List<Participante>> list() {
+		return ResponseEntity.ok(participanteService.list());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Participante> get(@PathVariable Integer id) {
+		return ResponseEntity.ok(participanteService.get(id));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Participante> create(@RequestBody Participante participante) {
+		Participante newParticipante = participanteService.create(participante);
+		return ResponseEntity.ok(newParticipante);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Participante> update(@PathVariable Integer id, @RequestBody Participante participante) {
+		Participante updatedParticipante = participanteService.update(id, participante);
+		return ResponseEntity.ok(updatedParticipante);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Participante> delete(@PathVariable Integer id) {
+		Participante deletedParticipante = participanteService.delete(id);
+		return ResponseEntity.ok(deletedParticipante);
+	}
 }
--- src/main/java/co/edu/ufps/services/ParticipanteService.java
+++ src/main/java/co/edu/ufps/services/ParticipanteService.java
@@ -1,8 +1,54 @@
 package co.edu.ufps.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import co.edu.ufps.entities.Participante;
+import co.edu.ufps.repositories.ParticipanteRepository;
 
 @Service
 public class ParticipanteService {
+	@Autowired
+	private ParticipanteRepository participanteRepository;
 
+	public List<Participante> list() {
+		return participanteRepository.findAll();
+	}
+	
+	public Participante get(Integer id) {
+		Optional<Participante> participanteOpt = participanteRepository.findById(id);
+		if (participanteOpt.isPresent()) {
+			return participanteOpt.get();
+		}
+		return null;
+	}
+
+	public Participante create(Participante participante) {
+		return participanteRepository.save(participante);
+	}
+
+	public Participante update(Integer id, Participante participante) {
+		Optional<Participante> participanteOpt = participanteRepository.findById(id);
+		if (!participanteOpt.isPresent()) {
+			return null;
+		}
+		Participante updatedParticipante = participanteOpt.get();
+		updatedParticipante.setNombre(participante.getNombre());
+		updatedParticipante.setColegio_id(participante.getColegio_id());
+		updatedParticipante.setTipo_documento_id(participante.getTipo_documento_id());
+		return participanteRepository.save(updatedParticipante);
+	}
+
+	public Participante delete(Integer id) {
+		Optional<Participante> participanteOpt = participanteRepository.findById(id);
+		if (participanteOpt.isPresent()) {
+			Participante participante = participanteOpt.get();
+			participanteRepository.delete(participante);
+			return participante;
+		}
+		return null;
+	}
 }
