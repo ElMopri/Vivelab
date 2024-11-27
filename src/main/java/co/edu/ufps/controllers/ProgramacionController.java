9fd144c4a2411c42d53b626820bd3a019e2c8567
ElMopri <alopeztriana1@gmail.com>
2024-11-26 22:58:50 -0500

    servicios de programacion

--- src/main/java/co/edu/ufps/controllers/ProgramacionController.java
+++ src/main/java/co/edu/ufps/controllers/ProgramacionController.java
@@ -1,10 +1,52 @@
 package co.edu.ufps.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.ufps.entities.Programacion;
+import co.edu.ufps.services.ProgramacionService;
 
 @RestController
 @RequestMapping("/api/programaciones")
 public class ProgramacionController {
-
+	@Autowired
+	private ProgramacionService programacionService;
+	
+	@GetMapping
+	public ResponseEntity<List<Programacion>> list() {
+		return ResponseEntity.ok(programacionService.list());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Programacion> get(@PathVariable Integer id) {
+		return ResponseEntity.ok(programacionService.get(id));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Programacion> create(@RequestBody Programacion programacion) {
+		Programacion newProgramacion = programacionService.create(programacion);
+		return ResponseEntity.ok(newProgramacion);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Programacion> update(@PathVariable Integer id, @RequestBody Programacion programacion) {
+		Programacion updatedProgramacion = programacionService.update(id, programacion);
+		return ResponseEntity.ok(updatedProgramacion);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Programacion> delete(@PathVariable Integer id) {
+		Programacion deletedProgramacion = programacionService.delete(id);
+		return ResponseEntity.ok(deletedProgramacion);
+	}
 }
--- src/main/java/co/edu/ufps/services/ProgramacionService.java
+++ src/main/java/co/edu/ufps/services/ProgramacionService.java
@@ -1,8 +1,61 @@
 package co.edu.ufps.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import co.edu.ufps.entities.Programacion;
+import co.edu.ufps.repositories.ProgramacionRepository;
 
 @Service
 public class ProgramacionService {
+	@Autowired
+	private ProgramacionRepository programacionRepository;
 
+	public List<Programacion> list() {
+		return programacionRepository.findAll();
+	}
+	
+	public Programacion get(Integer id) {
+		Optional<Programacion> programacionOpt = programacionRepository.findById(id);
+		if (programacionOpt.isPresent()) {
+			return programacionOpt.get();
+		}
+		return null;
+	}
+
+	public Programacion create(Programacion programacion) {
+		return programacionRepository.save(programacion);
+	}
+
+	public Programacion update(Integer id, Programacion programacion) {
+		Optional<Programacion> programacionOpt = programacionRepository.findById(id);
+		if (!programacionOpt.isPresent()) {
+			return null;
+		}
+		Programacion updatedProgramacion = programacionOpt.get();
+		updatedProgramacion.setColegio_id(programacion.getColegio_id());
+		updatedProgramacion.setTaller_id(programacion.getTaller_id());
+		updatedProgramacion.setFecha_inicio(programacion.getFecha_inicio());
+		updatedProgramacion.setFecha_fin(programacion.getFecha_fin());
+		updatedProgramacion.setCantidad(programacion.getCantidad());
+		updatedProgramacion.setObservacion(programacion.getObservacion());
+		updatedProgramacion.setInstructor_id(programacion.getInstructor_id());
+		updatedProgramacion.setGrado(programacion.getGrado());
+		updatedProgramacion.setGrupo(programacion.getGrupo());
+		updatedProgramacion.setUbicacion_id(programacion.getUbicacion_id());
+		return programacionRepository.save(updatedProgramacion);
+	}
+
+	public Programacion delete(Integer id) {
+		Optional<Programacion> programacionOpt = programacionRepository.findById(id);
+		if (programacionOpt.isPresent()) {
+			Programacion programacion = programacionOpt.get();
+			programacionRepository.delete(programacion);
+			return programacion;
+		}
+		return null;
+	}
 }
