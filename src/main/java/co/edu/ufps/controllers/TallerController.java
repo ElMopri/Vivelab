64dc763bc62e31170990873658e4c3803032eab1
ElMopri <alopeztriana1@gmail.com>
2024-11-26 22:00:10 -0500

    servicios de taller

--- src/main/java/co/edu/ufps/controllers/TallerController.java
+++ src/main/java/co/edu/ufps/controllers/TallerController.java
@@ -1,10 +1,52 @@
 package co.edu.ufps.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.ufps.entities.Taller;
+import co.edu.ufps.services.TallerService;
 
 @RestController
 @RequestMapping("/api/talleres")
 public class TallerController {
-
+	@Autowired
+	private TallerService tallerService;
+	
+	@GetMapping
+	public ResponseEntity<List<Taller>> list() {
+		return ResponseEntity.ok(tallerService.list());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Taller> get(@PathVariable Integer id) {
+		return ResponseEntity.ok(tallerService.get(id));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Taller> create(@RequestBody Taller taller) {
+		Taller newTaller = tallerService.create(taller);
+		return ResponseEntity.ok(newTaller);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Taller> update(@PathVariable Integer id, @RequestBody Taller tallerDetails) {
+		Taller updatedTaller = tallerService.update(id, tallerDetails);
+		return ResponseEntity.ok(updatedTaller);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Taller> delete(@PathVariable Integer id) {
+		Taller deletedTaller = tallerService.delete(id);
+		return ResponseEntity.ok(deletedTaller);
+	}
 }
--- src/main/java/co/edu/ufps/services/TallerService.java
+++ src/main/java/co/edu/ufps/services/TallerService.java
@@ -1,8 +1,53 @@
 package co.edu.ufps.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import co.edu.ufps.entities.Taller;
+import co.edu.ufps.repositories.TallerRepository;
 
 @Service
 public class TallerService {
+	@Autowired
+	private TallerRepository tallerRepository;
 
+	public List<Taller> list() {
+		return tallerRepository.findAll();
+	}
+	
+	public Taller get(Integer id) {
+		Optional<Taller> tallerOpt = tallerRepository.findById(id);
+		if (tallerOpt.isPresent()) {
+			return tallerOpt.get();
+		}
+		return null;
+	}
+
+	public Taller create(Taller taller) {
+		return tallerRepository.save(taller);
+	}
+
+	public Taller update(Integer id, Taller tallerDetails) {
+		Optional<Taller> tallerOpt = tallerRepository.findById(id);
+		if (!tallerOpt.isPresent()) {
+			return null;
+		}
+		Taller taller = tallerOpt.get();
+		taller.setNombre(tallerDetails.getNombre());
+		taller.setDescripcion(tallerDetails.getDescripcion());
+		return tallerRepository.save(taller);
+	}
+
+	public Taller delete(Integer id) {
+		Optional<Taller> tallerOpt = tallerRepository.findById(id);
+		if (tallerOpt.isPresent()) {
+			Taller taller = tallerOpt.get();
+			tallerRepository.delete(taller);
+			return taller;
+		}
+		return null;
+	}
 }
