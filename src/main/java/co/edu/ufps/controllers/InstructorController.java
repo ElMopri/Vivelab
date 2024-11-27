acf1421970bc977aa9345806c898a4ccebe3633c
ElMopri <alopeztriana1@gmail.com>
2024-11-26 22:23:08 -0500

    servicios de instrutor

--- src/main/java/co/edu/ufps/controllers/InstructorController.java
+++ src/main/java/co/edu/ufps/controllers/InstructorController.java
@@ -1,10 +1,52 @@
 package co.edu.ufps.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.ufps.entities.Instructor;
+import co.edu.ufps.services.InstructorService;
 
 @RestController
 @RequestMapping("/api/instructores")
 public class InstructorController {
-
+	@Autowired
+	private InstructorService instructorService;
+	
+	@GetMapping
+	public ResponseEntity<List<Instructor>> list() {
+		return ResponseEntity.ok(instructorService.list());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Instructor> get(@PathVariable Integer id) {
+		return ResponseEntity.ok(instructorService.get(id));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Instructor> create(@RequestBody Instructor instructor) {
+		Instructor newInstructor = instructorService.create(instructor);
+		return ResponseEntity.ok(newInstructor);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Instructor> update(@PathVariable Integer id, @RequestBody Instructor instructorDetails) {
+		Instructor updatedInstructor = instructorService.update(id, instructorDetails);
+		return ResponseEntity.ok(updatedInstructor);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Instructor> delete(@PathVariable Integer id) {
+		Instructor deletedInstructor = instructorService.delete(id);
+		return ResponseEntity.ok(deletedInstructor);
+	}
 }
--- src/main/java/co/edu/ufps/services/InstructorService.java
+++ src/main/java/co/edu/ufps/services/InstructorService.java
@@ -1,8 +1,53 @@
 package co.edu.ufps.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import co.edu.ufps.entities.Instructor;
+import co.edu.ufps.repositories.InstructorRepository;
 
 @Service
 public class InstructorService {
+	@Autowired
+	private InstructorRepository instructorRepository;
 
+	public List<Instructor> list() {
+		return instructorRepository.findAll();
+	}
+	
+	public Instructor get(Integer id) {
+		Optional<Instructor> instructorOpt = instructorRepository.findById(id);
+		if (instructorOpt.isPresent()) {
+			return instructorOpt.get();
+		}
+		return null;
+	}
+
+	public Instructor create(Instructor instructor) {
+		return instructorRepository.save(instructor);
+	}
+
+	public Instructor update(Integer id, Instructor instructorDetails) {
+		Optional<Instructor> instructorOpt = instructorRepository.findById(id);
+		if (!instructorOpt.isPresent()) {
+			return null;
+		}
+		Instructor instructor = instructorOpt.get();
+		instructor.setNombre(instructorDetails.getNombre());
+		instructor.setDocumento(instructorDetails.getDocumento());
+		return instructorRepository.save(instructor);
+	}
+
+	public Instructor delete(Integer id) {
+		Optional<Instructor> instructorOpt = instructorRepository.findById(id);
+		if (instructorOpt.isPresent()) {
+			Instructor instructor = instructorOpt.get();
+			instructorRepository.delete(instructor);
+			return instructor;
+		}
+		return null;
+	}
 }
