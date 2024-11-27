5096338053b1170ff9999bea02ff4a42e586797c
ElMopri <alopeztriana1@gmail.com>
2024-11-26 23:04:27 -0500

    cambios en el WebConfig

--- src/main/java/co/edu/ufps/configurations/WebConfig.java
+++ src/main/java/co/edu/ufps/configurations/WebConfig.java
@@ -2,16 +2,18 @@ package co.edu.ufps.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*") 
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
