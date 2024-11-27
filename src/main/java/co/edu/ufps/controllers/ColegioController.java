f6634f504c5b4db056129d8baf9d4245161912ab
ElMopri <alopeztriana1@gmail.com>
2024-11-26 22:49:06 -0500

    servicios de colegio

--- src/main/java/co/edu/ufps/controllers/ColegioController.java
+++ src/main/java/co/edu/ufps/controllers/ColegioController.java
@@ -1,10 +1,52 @@
 package co.edu.ufps.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.ufps.entities.Colegio;
+import co.edu.ufps.services.ColegioService;
 
 @RestController
 @RequestMapping("/api/colegios")
 public class ColegioController {
-
+	@Autowired
+	private ColegioService colegioService;
+	
+	@GetMapping
+	public ResponseEntity<List<Colegio>> list() {
+		return ResponseEntity.ok(colegioService.list());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Colegio> get(@PathVariable Integer id) {
+		return ResponseEntity.ok(colegioService.get(id));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Colegio> create(@RequestBody Colegio colegio) {
+		Colegio newColegio = colegioService.create(colegio);
+		return ResponseEntity.ok(newColegio);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Colegio> update(@PathVariable Integer id, @RequestBody Colegio colegio) {
+		Colegio updatedColegio = colegioService.update(id, colegio);
+		return ResponseEntity.ok(updatedColegio);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Colegio> delete(@PathVariable Integer id) {
+		Colegio deletedColegio = colegioService.delete(id);
+		return ResponseEntity.ok(deletedColegio);
+	}
 }
--- src/main/java/co/edu/ufps/services/ColegioService.java
+++ src/main/java/co/edu/ufps/services/ColegioService.java
@@ -1,8 +1,54 @@
 package co.edu.ufps.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import co.edu.ufps.entities.Colegio;
+import co.edu.ufps.repositories.ColegioRepository;
 
 @Service
 public class ColegioService {
+	@Autowired
+	private ColegioRepository colegioRepository;
 
+	public List<Colegio> list() {
+		return colegioRepository.findAll();
+	}
+	
+	public Colegio get(Integer id) {
+		Optional<Colegio> colegioOpt = colegioRepository.findById(id);
+		if (colegioOpt.isPresent()) {
+			return colegioOpt.get();
+		}
+		return null;
+	}
+
+	public Colegio create(Colegio colegio) {
+		return colegioRepository.save(colegio);
+	}
+
+	public Colegio update(Integer id, Colegio colegio) {
+		Optional<Colegio> colegioOpt = colegioRepository.findById(id);
+		if (!colegioOpt.isPresent()) {
+			return null;
+		}
+		Colegio updatedColegio = colegioOpt.get();
+		updatedColegio.setNombre(colegio.getNombre());
+		updatedColegio.setMunicipio_id(colegio.getMunicipio_id());
+		updatedColegio.setDane(colegio.getDane());
+		return colegioRepository.save(updatedColegio);
+	}
+
+	public Colegio delete(Integer id) {
+		Optional<Colegio> colegioOpt = colegioRepository.findById(id);
+		if (colegioOpt.isPresent()) {
+			Colegio colegio = colegioOpt.get();
+			colegioRepository.delete(colegio);
+			return colegio;
+		}
+		return null;
+	}
 }
