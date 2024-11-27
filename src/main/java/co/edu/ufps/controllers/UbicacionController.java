03a6acfbb0489b7143d578a6f4a5bb94705cadfd
ElMopri <alopeztriana1@gmail.com>
2024-11-26 22:25:51 -0500

    servicios de ubicacion

--- src/main/java/co/edu/ufps/controllers/UbicacionController.java
+++ src/main/java/co/edu/ufps/controllers/UbicacionController.java
@@ -1,10 +1,52 @@
 package co.edu.ufps.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.ufps.entities.Ubicacion;
+import co.edu.ufps.services.UbicacionService;
 
 @RestController
 @RequestMapping("/api/ubicaciones")
 public class UbicacionController {
-
+	@Autowired
+	private UbicacionService ubicacionService;
+	
+	@GetMapping
+	public ResponseEntity<List<Ubicacion>> list() {
+		return ResponseEntity.ok(ubicacionService.list());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Ubicacion> get(@PathVariable Integer id) {
+		return ResponseEntity.ok(ubicacionService.get(id));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Ubicacion> create(@RequestBody Ubicacion ubicacion) {
+		Ubicacion newUbicacion = ubicacionService.create(ubicacion);
+		return ResponseEntity.ok(newUbicacion);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Ubicacion> update(@PathVariable Integer id, @RequestBody Ubicacion ubicacionDetails) {
+		Ubicacion updatedUbicacion = ubicacionService.update(id, ubicacionDetails);
+		return ResponseEntity.ok(updatedUbicacion);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Ubicacion> delete(@PathVariable Integer id) {
+		Ubicacion deletedUbicacion = ubicacionService.delete(id);
+		return ResponseEntity.ok(deletedUbicacion);
+	}
 }
--- src/main/java/co/edu/ufps/services/UbicacionService.java
+++ src/main/java/co/edu/ufps/services/UbicacionService.java
@@ -1,8 +1,52 @@
 package co.edu.ufps.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import co.edu.ufps.entities.Ubicacion;
+import co.edu.ufps.repositories.UbicacionRepository;
 
 @Service
 public class UbicacionService {
+	@Autowired
+	private UbicacionRepository ubicacionRepository;
 
+	public List<Ubicacion> list() {
+		return ubicacionRepository.findAll();
+	}
+	
+	public Ubicacion get(Integer id) {
+		Optional<Ubicacion> ubicacionOpt = ubicacionRepository.findById(id);
+		if (ubicacionOpt.isPresent()) {
+			return ubicacionOpt.get();
+		}
+		return null;
+	}
+
+	public Ubicacion create(Ubicacion ubicacion) {
+		return ubicacionRepository.save(ubicacion);
+	}
+
+	public Ubicacion update(Integer id, Ubicacion ubicacionDetails) {
+		Optional<Ubicacion> ubicacionOpt = ubicacionRepository.findById(id);
+		if (!ubicacionOpt.isPresent()) {
+			return null;
+		}
+		Ubicacion ubicacion = ubicacionOpt.get();
+		ubicacion.setNombre(ubicacionDetails.getNombre());
+		return ubicacionRepository.save(ubicacion);
+	}
+
+	public Ubicacion delete(Integer id) {
+		Optional<Ubicacion> ubicacionOpt = ubicacionRepository.findById(id);
+		if (ubicacionOpt.isPresent()) {
+			Ubicacion ubicacion = ubicacionOpt.get();
+			ubicacionRepository.delete(ubicacion);
+			return ubicacion;
+		}
+		return null;
+	}
 }
